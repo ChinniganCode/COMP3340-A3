e8c58a99af9789c59543b1c1c47b197ae96a3864
FinbarL <finbar.laffan@mudbath.com.au>
2023-10-09 21:51:20 +1100

    inital testing with dataset

--- Exercise-1.R
+++ Exercise-1.R
@@ -1,20 +1,13 @@
-install.packages("cccd")
-install.packages("igraph")
 library("cccd")
 library(datasets)
-library(help = "datasets")
-x <- matrix(runif(100),ncol=2)
 
-g <- rng(x)
-## Not run:
-plot(g)
 
-## End(Not run)
-
-## Example using 'open':
-g <- graph.full(5,directed=FALSE)
-
-g1 <- rng(x=get.adjacency(g,sparse=FALSE),open=TRUE)
-ecount(g1)
-g2 <- rng(x=get.adjacency(g,sparse=FALSE),open=FALSE)
-graph.isomorphic(g2,g)
+redwine_dataset <- read.csv("datasets/winequality-red.csv", header=TRUE, sep=";")
+#
+ head(redwine_dataset)
+matrix <- as.matrix(redwine_dataset)
+head(matrix)
+print("RUN TRAINING")
+ rng_graph <- rng(x=matrix, method="euclidean", algorithm = 'cover_tree', k=2, open=TRUE, r = 0.1)
+layout <- layout_with_fr(rng_graph)
+plot(rng_graph, layout = layout)
